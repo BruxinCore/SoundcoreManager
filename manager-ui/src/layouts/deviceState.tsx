c23f8d471c55444e48a83a4fc44bd0dfa4125d08
Bruxin <66227898+BruxinCore@users.noreply.github.com>
2026-09-09 18:27:23 -0300

    fix(ui): add SoundModeTabs back to device state layout

--- manager-ui/src/layouts/deviceState.tsx
+++ manager-ui/src/layouts/deviceState.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { DeviceStateCard } from '@components/DeviceStateCard/deviceStateCard';
 import { SoundcoreDeviceState } from '@generated-types/soundcore-lib';
 import { EqualizerCard } from '@components/EqualizerCard/equalizerCard';
+import { SoundModeTabs } from '@components/DeviceStateCard/soundModeTabs';
 
 export const DeviceStateLayout: React.FC<{ state: SoundcoreDeviceState }> = ({ state }) => {
   return (
     <>
       <DeviceStateCard state={state} />
+      <SoundModeTabs state={state} />
       <EqualizerCard state={state} />
     </>
   );
