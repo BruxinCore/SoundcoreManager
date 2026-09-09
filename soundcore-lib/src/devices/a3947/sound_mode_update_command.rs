17ffc0b9b933ed43551f75751a4efced1e3891ba
Neroki <66227898+BruxinCore@users.noreply.github.com>
2026-09-08 23:21:46 -0300

    feat: Add A3947 support

---
+++ soundcore-lib/src/devices/a3947/sound_mode_update_command.rs
@@ -0,0 +1,31 @@
+use crate::{models::SoundMode, packets::Packet};
+
+pub struct A3947SoundModeUpdateCommand {
+    sound_mode: SoundMode,
+}
+
+impl A3947SoundModeUpdateCommand {
+    pub fn new(sound_mode: SoundMode) -> Self {
+        Self { sound_mode }
+    }
+}
+
+impl Packet for A3947SoundModeUpdateCommand {
+    fn command(&self) -> [u8; 7] {
+        [0x08, 0xEE, 0x00, 0x00, 0x00, 0x06, 0x81]
+    }
+
+    fn payload(&self) -> Vec<u8> {
+        vec![
+            0x11,
+            0x00,
+            self.sound_mode.current.as_u8(),
+            (self.sound_mode.custom_anc.as_u8() << 4) | 0x00, 
+            self.sound_mode.trans_mode.as_u8(),
+            self.sound_mode.anc_mode.as_u8(),
+            0x01, 
+            0x01, 
+            0x00,
+        ]
+    }
+}
